dbb34d3decba7d643babd667b569af8fa6bbbf17
MinhTalent <vcb000111@gmail.com>
2021-01-14 11:11:17 +0700

    ok

--- database/seeds/CateSeeder.php
+++ database/seeds/CateSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CateSeeder extends Seeder
 {
@@ -11,6 +12,60 @@ class CateSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+
+        DB::table('cates')->delete();
+
+        DB::table('cates')->insert(array(
+            0 =>
+            array(
+                'id' => 1,
+                'cate_name' => 'Drama',
+                'created_at' => '2021-01-06 01:15:40',
+                'updated_at' => '2021-01-06 01:15:40',
+            ),
+            1 =>
+            array(
+                'id' => 2,
+                'cate_name' => 'No-drama',
+                'created_at' => '2021-01-06 01:16:11',
+                'updated_at' => '2021-01-06 01:16:11',
+            ),
+            2 =>
+            array(
+                'id' => 3,
+                'cate_name' => '1-shot',
+                'created_at' => '2021-01-06 01:16:20',
+                'updated_at' => '2021-01-06 01:16:20',
+            ),
+            3 =>
+            array(
+                'id' => 4,
+                'cate_name' => 'Lesbian',
+                'created_at' => '2021-01-06 01:16:28',
+                'updated_at' => '2021-01-06 01:16:28',
+            ),
+            4 =>
+            array(
+                'id' => 5,
+                'cate_name' => 'Wall, desk, chair',
+                'created_at' => '2021-01-06 01:16:38',
+                'updated_at' => '2021-01-06 01:16:38',
+            ),
+            5 =>
+            array(
+                'id' => 6,
+                'cate_name' => 'Other-uncen',
+                'created_at' => '2021-01-06 01:16:50',
+                'updated_at' => '2021-01-06 01:16:50',
+            ),
+            6 =>
+            array(
+                'id' => 7,
+                'cate_name' => 'Other-cen',
+                'created_at' => '2021-01-06 01:16:59',
+                'updated_at' => '2021-01-06 01:16:59',
+            ),
+        ));
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(CateSeeder::class);
+        $this->call(MoviesTableSeeder::class);
     }
 }
